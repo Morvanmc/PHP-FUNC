f3507f1fe6e40d3a3db41ac6a33269cfc73a154c
Morvan Marques <morvanmc@outlook.com>
2022-06-06 16:12:13 -0300

    Update func.php

--- func.php
+++ func.php
@@ -6,16 +6,27 @@
         $pendingTransactions = DB::Table('withdrawals_transactions')->where('status', 'PENDENTE')->get();
 
         if (count($pendingTransactions) > 0) {
+
             $limit = 50;
 
             foreach ($pendingTransactions as $pendingTransaction) {
-                if ($pendingTransaction->amount <= $limit) {
-                    WithdrawalsController::updateWithdrawals($pendingTransaction->user_id, 'PROCESS');
-                    
-                    $response = WithdrawalsController::requestPix($pendingTransaction);
+                $cpf = DB::Table('user')->where('cpf', $pendingTransaction->cpf)->first();
 
-                    if ($response == 200) {
-                        WithdrawalsController::updateWithdrawals($pendingTransaction->user_id, 'WAITING');
+                if ($cpf) {
+                    $account = DB::Table('accounts')->where('user_id', $pendingTransaction->user_id)->latest();
+
+                    if ($pendingTransaction->amount <= $limit && $pendingTransaction->amount <= $account->balance) {
+                        WithdrawalsController::updateWithdrawals($pendingTransaction->user_id, 'PROCESS');
+
+                        $response = WithdrawalsController::requestPix($pendingTransaction);
+
+                        if ($response == 200) {
+                            WithdrawalsController::updateWithdrawals($pendingTransaction->user_id, 'WAITING');
+                            //fazer insert em uma table cashout gerencianet (e2E, id_withdrawals_transaction, status)
+                            //webhook(e2E)-> compara na cashout-> muda status na withdrawals e na cashout
+                        } else {
+                            WithdrawalsController::updateWithdrawals($pendingTransaction->user_id, 'REVISION');
+                        }
                     } else {
                         WithdrawalsController::updateWithdrawals($pendingTransaction->user_id, 'REVISION');
                     }
@@ -32,18 +43,43 @@
      * updateWithdrawals
      */
 
-     public function updateWithdrawals($user_id, $status)
-     {
-        DB::table('withdrawals_transactions')->where('user_id',$user_id)->update(['status' => $status]);
-     }
+    public function updateWithdrawals($user_id, $status)
+    {
+        DB::table('withdrawals_transactions')->where('user_id', $user_id)->update(['status' => $status]);
+    }
 
+    /**
+     * getISPB
+     */
+    public function getISPB()
+    {
+        $url = 'https://brasilapi.com.br/api/banks/v1/063';
+        $headers = [
+            'Content-type: application/json'
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => $headers
+        ]);
+
+        $response = curl_exec($curl);
+        $response = json_decode($response, true);
+
+        curl_close($curl);
+
+        return $response;
+    }
     /**
      * Call PIX API Gerencianet
      */
 
-    public function requestPix()
+    public function requestPix($transaction)
     {
-        $tokens = DB::Table('tokens')->where('type', 'hub2b')->first();
+        $tokens = DB::Table('tokens')->where('type', 'gerencia_net')->first();
         if ($tokens) {
             $token = $tokens->token;
         }
@@ -58,18 +94,19 @@
         $collection_name = 'v2/pix';
         $certificate =  __DIR__ . env('CERTIFICATE');
         $endPoint = $url . '/' . $collection_name;
-        print_r($endPoint);
-        exit;
+
+        $ispb = WithdrawalsController::getISPB($transaction->bank_code);
+
         $data = [
             "valor" => "99.99",
             "pagador" => [
-                "chave" => "19974764017",
+                "chave" => env('CLIENT_PIX_KEY'),
                 "infoPagador" => "Segue o pagamento da conta"
             ],
             "favorecido" => [
                 "nome" => "JOSE CARVALHO",
                 "cpf" => "10519952057",
-                "codigoBanco" => "09089356",
+                "codigoBanco" => $ispb,
                 "agencia" => "1",
                 "conta" => "123453",
                 "tipoConta" => "cacc"
@@ -89,8 +126,46 @@
 
         $response = curl_exec($curl);
         $response = json_decode($response, true);
-        
+
         curl_close($curl);
 
-        return $response;     
+        return $response;
     }
+
+    /**
+     * Webhook Gerencianet
+     */
+    /*public function webhookResponse()
+    {
+        try {
+            $api = new Gerencianet();
+            $callbackNotification = $api->getNotification();
+
+            if (count($callbackNotification) > 0) {
+                $e2E = $callbackNotification->$pix->endToEndId;
+
+                if ($callbackNotification->$pix->status == "REALIZADO") {
+                    $payment = DB::Table('user_payments')->where('endToEndId', $e2E)->get();
+
+                    DB::table('withdrawals_transactions')->where('user_id', $payment->user_id)->update(['status' => 'PAID']);
+                } else {
+                    $payment = DB::Table('user_payments')->where('endToEndId', $e2E)->get();
+
+                    DB::table('withdrawals_transactions')->where('user_id', $payment->user_id)->update(['status' => 'REVISION']);
+                }
+            } else {
+                return "Não há atualizações!";
+            }
+        } catch (GerencianetException $e) {
+            print_r($e->code);
+            print_r($e->error);
+            print_r($e->errorDescription);
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        }
+    }*/
+                        
+                        //Route::post('pix', [PixController::class, 'webhookPixCashout']);
+                         Route::get('validates', [WithdrawalsController::class, 'validateWithdrawals']);
+        Route::post('withdrawalsPix', [WithdrawalsController::class, 'requestPix']);
+        Route::get('ispb', [WithdrawalsController::class, 'getISPB']);
